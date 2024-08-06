ec1a8174cf11356b68d2b5e9495b96989dc6e821
Hamdan Hamdu <hamdanhamdu@Hamdans-MacBook-Air.local>
2024-08-07 00:34:24 +0530

    updated

--- src/views/Test.jsx
+++ src/views/Test.jsx
@@ -1,48 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
-import WaveSurfer from "wavesurfer.js";
-import sampleAudio from "/public/audio/audio1.mp3";
 
-const Waveform = ({ audio }) => {
-  const containerRef = useRef();
-  const waveSurferRef = useRef({
-    isPlaying: () => false,
-  });
-  const [isPlaying, toggleIsPlaying] = useState(false);
-
-  useEffect(() => {
-    const waveSurfer = WaveSurfer.create({
-      container: containerRef.current,
-      responsive: true,
-      fillParent: false,
-      cursorWidth: 0,
-      barWidth: 2,
-      barHeight: 10,
-      minPxPerSec: 10,
-    });
-    waveSurfer.load(sampleAudio);
-    waveSurfer.on("ready", () => {
-      waveSurferRef.current = waveSurfer;
-    });
-
-    return () => {
-      waveSurfer.destroy();
-    };
-  }, [sampleAudio]);
-
+const Test = ({ }) => {
   return (
     <>
-      <button
-        onClick={() => {
-          waveSurferRef.current.playPause();
-          toggleIsPlaying(waveSurferRef.current.isPlaying());
-        }}
-        type="button"
-      >
-        {isPlaying ? "pause" : "play"}
-      </button>
-      <div ref={containerRef} />
+      <div>hello</div>
     </>
   );
 };
 
-export default Waveform;
+export default Test;
