2ef40a215a112eb12f6eda36ff12b9ab1de1412d
Hamdan KT <hamdankt@onlinesafe.in>
2024-11-18 15:10:31 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -61,6 +61,13 @@ export default defineConfig({
 	build: {
 		outDir: "dist",
 		sourcemap: true,
+		rollupOptions: {
+			output: {
+				entryFileNames: "assets/[name].[hash].js",
+				chunkFileNames: "assets/[name].[hash].js",
+				assetFileNames: "assets/[name].[hash].[ext]",
+			},
+		},
 	},
 	resolve: {
 		alias: {
