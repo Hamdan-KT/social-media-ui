181f167366bf24d1c3b3e1534e9d95a1e0a07017
Hamdan KT <hamdankt@onlinesafe.in>
2025-05-09 18:04:05 +0530

    updated push notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,65 +1,30 @@
-// public/firebase-messaging-sw.js
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js");
-importScripts(
-	"https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js"
-);
+import { initializeApp } from "firebase/app";
+import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyCxOzLhnc5BHEGB9ViORxVvhGXlOjsmz2o",
 	authDomain: "instogram-786.firebaseapp.com",
 	projectId: "instogram-786",
-	storageBucket: "instogram-786.firebasestorage.app",
-	messagingSenderId: "21381023072",
+	messagingSenderId: 21381023072,
 	appId: "1:21381023072:web:3cdf146b1825d34f2b0a05",
-	measurementId: "G-3XBRJKWGY8",
 };
 
-firebase.initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
-const messaging = firebase.messaging();
+const messaging = getMessaging();
 
-messaging.onBackgroundMessage(function (payload) {
+onBackgroundMessage(messaging, (payload) => {
+	const { title, body } = payload.notification;
+	console.log("got notification ....______d-----df-_")
 	console.log(
 		"[firebase-messaging-sw.js] Received background message ",
 		payload
 	);
-
-	const { title, body } = payload.notification;
-
+	// Customize notification here
+	const notificationTitle = "Background Message Title";
 	const notificationOptions = {
-		body,
-		// icon: "/logo192.png", // Optional: Add app icon
+		body: "Background Message body.",
+		// icon: "/firebase-logo.png",
 	};
-
-	self.registration.showNotification(title, notificationOptions);
+	self.registration.showNotification(notificationTitle, notificationOptions);
 });
-// import { initializeApp } from "firebase/app";
-// import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
-
-// const firebaseConfig = {
-// 	apiKey: "AIzaSyCxOzLhnc5BHEGB9ViORxVvhGXlOjsmz2o",
-// 	authDomain: "instogram-786.firebaseapp.com",
-// 	projectId: "instogram-786",
-// 	messagingSenderId: 21381023072,
-// 	appId: "1:21381023072:web:3cdf146b1825d34f2b0a05",
-// };
-
-// initializeApp(firebaseConfig);
-
-// const messaging = getMessaging();
-
-// onBackgroundMessage(messaging, (payload) => {
-// 	const { title, body } = payload.notification;
-// 	console.log("got notification ....______d-----df-_")
-// 	console.log(
-// 		"[firebase-messaging-sw.js] Received background message ",
-// 		payload
-// 	);
-// 	// Customize notification here
-// 	const notificationTitle = "Background Message Title";
-// 	const notificationOptions = {
-// 		body: "Background Message body.",
-// 		// icon: "/firebase-logo.png",
-// 	};
-// 	self.registration.showNotification(notificationTitle, notificationOptions);
-// });
