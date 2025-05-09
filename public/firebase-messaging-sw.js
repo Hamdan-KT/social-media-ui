f1fde7d273b728c0bae8337fcda5079adc98834c
Hamdan KT <hamdankt@onlinesafe.in>
2025-05-09 20:53:11 +0530

    updated sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -31,9 +31,9 @@ messaging.onBackgroundMessage(function (payload) {
 		payload
 	);
 	// Customize notification here
-	const notificationTitle = "Background Message Title";
+	const notificationTitle = title ?? "Background Message Title";
 	const notificationOptions = {
-		body: "Background Message body.",
+		body: body ?? "Background Message body.",
 		// icon: "/firebase-logo.png",
 	};
 	self.registration.showNotification(notificationTitle, notificationOptions);
